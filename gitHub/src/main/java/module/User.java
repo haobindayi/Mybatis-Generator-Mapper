fdd364d6baafa4f8773608967c0b1e1ea8bfdf13
JulyTail <32617871+haobindayi@users.noreply.github.com>
2019-12-27 12:36:39 +0800

    简易的Mybatis的自动生成文件工程

---
+++ gitHub/src/main/java/module/User.java
@@ -0,0 +1,53 @@
+package module;
+
+public class User {
+    private String id;
+
+    private String username;
+
+    private String password;
+
+    private Integer isdel;
+
+    private String sex;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public Integer getIsdel() {
+        return isdel;
+    }
+
+    public void setIsdel(Integer isdel) {
+        this.isdel = isdel;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex == null ? null : sex.trim();
+    }
+}
